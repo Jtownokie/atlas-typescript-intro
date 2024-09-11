bc4c3b95855b9263daadae5d182da017626cbd91
Jtownokie <christopher.stephens@atlasschool.com>
2024-09-10 19:08:09 -0500

    Moved shuffle toggle to app context to keep track of state for shuffling songs

--- src/MusicPlayer.tsx
+++ src/MusicPlayer.tsx
@@ -1,27 +1,33 @@
 // Music Player Component
 import CurrentlyPlaying from "./components/CurrentlyPlaying";
 import Playlist from "./components/Playlist";
-import { usePlaylistData } from "./hooks/usePlaylistData";
 import { createContext } from "react";
 import { SongObject } from "./components/Playlist";
 import { PuffLoader } from "react-spinners";
 import { useSongSelect } from "./hooks/useSongSelect";
+import { usePlaylistData } from "./hooks/usePlaylistData";
+import { useShuffleToggle } from "./hooks/useShuffleToggle";
 
 export type AppContextType = {
   data: [SongObject] | undefined;
   currentSong: number | undefined;
-  handleSongSelect: (id: number) => void | undefined;
+  handleSongSelect: (id: number) => void;
+  shuffle: boolean;
+  handleShuffleToggle: () => void;
 };
 
 export const AppContext = createContext<AppContextType>({
   data: undefined,
   currentSong: undefined,
   handleSongSelect: () => {},
+  shuffle: false,
+  handleShuffleToggle: () => {}
 });
 
 export function MusicPlayer() {
   const { data, loading } = usePlaylistData();
   const { currentSong, handleSongSelect } = useSongSelect();
+  const { shuffle, handleShuffleToggle } = useShuffleToggle();
 
   if (loading) {
     return (
@@ -32,7 +38,7 @@ export function MusicPlayer() {
   }
 
   return (
-    <AppContext.Provider value={{ data, currentSong, handleSongSelect }}>
+    <AppContext.Provider value={{ data, currentSong, handleSongSelect, shuffle, handleShuffleToggle }}>
       <div className="mx-auto flex h-full w-full max-w-4xl flex-col rounded-lg bg-burgundy shadow-xl md:flex-row">
         <CurrentlyPlaying />
         <Playlist />
---
+++ src/hooks/useShuffleToggle.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+
+export function useShuffleToggle(){
+  const [shuffle, toggleShuffle] = useState<boolean>(false);
+
+  const handleShuffleToggle = () => {
+    if (shuffle === false) {
+      toggleShuffle(true);
+    } else if (shuffle === true) {
+      toggleShuffle(false);
+    }
+  };
+
+  return { shuffle, handleShuffleToggle };
+}
